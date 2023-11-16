d458b18d9916a47f5426bc02c60622e2b7210c21
Charles Dieterle <cdieterle33@gmail.com>
2023-11-16 17:34:05 -0600

    Create hard-coded structure of app in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 function App() {
   return (
-    null
+    <div className='rebalanceapp'>
+      <h1>Investment Rebalancer</h1>
+      <h2>Investment List</h2>
+      <p>Enter your investments below, along with their current value and target percentage</p>
+      <p>Read about rebalancing TODO LINK NEEDED &lt;here&gt; for information on how to use this</p>
+      <ul>
+        <li>
+          <input defaultValue='Investment Name (e.g. AAPL)'></input>
+          <input defaultValue='Current Value'></input>
+          <input defaultValue='Target Percentage'></input>
+          <button className='button deleteinvestment'>Delete this investment</button>
+        </li>
+        <li>
+          <button className='button addinvestment'>Add a new investment</button>
+        </li>
+      </ul>
+
+    </div>
   );
 }
 
